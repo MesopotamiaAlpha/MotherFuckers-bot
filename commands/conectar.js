077b3f6fc666f074df8dc78927f27882e5667d8d
MesopotamiaAlpha <leao-gomes@hotmail.com>
2018-05-25 15:36:36 -0300

    Correção
    Correção no arquivo

--- commands/conectar.js
+++ commands/conectar.js
@@ -1,10 +1,10 @@
 exports.run = (client, message, args) => {
 
   // Pegar o nome do canal pelo ID
-  let channel = client.channels.get('421445329641013258');
+  let channel = client.channels.get('aqui_vai_entrar_o_id_da_sala');
 
   // ou pelo nome (menos persistente)
-  channel = client.channels.find('name', 'Geral');
+  channel = client.channels.find('name', 'aqui_entra_o_nome_da_sala');
   channel.join()
 
   //aqui estou tentando pegar o nome da sala e mostar no console,mas nao esta funcionando, esta aparecendo undefined

26f5c3021be81cf56dad8e9113a2b444a0405ba3
MesopotamiaAlpha <leao-gomes@hotmail.com>
2018-05-25 15:41:58 -0300

    Correção
    Correção na configuração

--- commands/desconectar.js
+++ commands/desconectar.js
@@ -1,7 +1,7 @@
 exports.run = (client, message, args) => {
 
-	let channel = client.channels.get('421445329641013258');
-channel = client.channels.find('name', 'Geral');
+	let channel = client.channels.get('aqui_entra_o');
+channel = client.channels.find('name', 'aqui_entra_o_nome_da_sala');
   channel.leave()
   console.log('Bot desconectado do canal de audio'.red)
   
